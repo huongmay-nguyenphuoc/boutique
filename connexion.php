3270a2a0ed86d275a024529d2010455b3c95a487
Cordial <alicia.cordial@laplateforme.io>
2021-02-08 11:01:06 +0100

    connexion check

--- connexion.php
+++ connexion.php
@@ -5,17 +5,22 @@ require_once 'classes/validator.php';
 session_start();
 
 
-if (isset($_POST['formconnexion'])) {
+if (isset($_POST['submit'])) {
 
     $validator = new validator();
+
     $login = htmlspecialchars($_POST['login']);
     $password = htmlspecialchars($_POST['password']);
 
     if ($validator->passwordConnect($login, $password) == 0) {
-        $error = "Vérifiez votre login ou votre mot de passe.";
+        
+        $error = "False Login or password";
+
     } else {
+
         $user = new user();
         $user->connect($login);
+        
         $_SESSION['user'] = $user;
         header("Location: profil.php");
        
@@ -24,6 +29,26 @@ if (isset($_POST['formconnexion'])) {
 }
 
 ?>
+
+
+<html lang="en">
+
+
+<main class="valign-wrapper">
+    <div class="row">
+        <h3 class="center"><em>Connexion</em></h3>
+
+        <!--Alerte (erreur ou succès)-->
+        <?php if (isset($error)): ?>
+            <div>
+                <p class="red-text"><?php echo $error; ?></p>
+            </div>
+        <?php elseif (isset($success)): ?>
+            <div>
+                <p class="red-text"><?php echo $success; ?></p>
+            </div>
+        <?php endif; ?>
+
 
 
         <!--Alerte (erreur ou succès)-->
@@ -46,5 +71,10 @@ if (isset($_POST['formconnexion'])) {
     <label for="password">Password</label><br>
     <input type="text" id="password" name="password"><br><br>
  
-     <input type="submit" value="submit">
+    <button class="btn waves-effect waves-light black " type="submit" name="submit">
+                <i class="material-icons right">send</i>
+            </button>
 </form>
+
+</main>
+</html>
