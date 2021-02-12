63d4fa064329964021ad347bc1574eb0ddf12ffa
huongmay-nguyenphuoc <huongmay-nguyenphuoc@laplateforme.io>
2021-02-12 15:42:55 +0100

    pushpush

--- classes/user.php
+++ classes/user.php
@@ -147,14 +147,55 @@ class user
     //RECUPERER HISTORIC
     public function cartHistoric()
     {
-        $cartHistoric = $this->pdo->Select("SELECT id_member, id_order, amount, date_register, state FROM order WHERE id_member = :id_member ORDER BY order.state DESC",
+        $cartHistoric = $this->pdo->Select("SELECT id_order, amount, date_register, state FROM `order` WHERE id_member = :id_member ORDER BY order.state DESC",
             ['id_member' => $this->id_member]);
         return $cartHistoric;
     }
 
 
+    public function cartHistoricdetails($id_order)
+    {
+        $details = $this->pdo->Select('SELECT * from `order_details` WHERE id_order = :id_order',
+            ['id_order' => $id_order]);
+        return $details;
+    }
 
     //SUPPRIMER COMPTE????
 
 
 }
+
+$user = new user();
+$user->connect('may');
+var_dump($user);
+$products = $user->cartHistoric();
+var_dump($user->cartHistoricdetails(26));
+
+/*Traitement bouton description*/
+if (isset($_POST['show'])) {
+    $show[$_POST["value"]] = true;
+}
+
+
+?>
+
+<ul>
+        <?php $i = 0; ?>
+        <?php foreach ($products as $product) : ?>
+            <li>
+                <ul>
+                    <li><?= $product['id_order'] ?></li>
+                    <li>
+                        <form method="post">
+                            <input type="hidden" name="value" value="<?= $i ?>">
+                            <input type="submit" name="show" value="show"></form>
+                    </li>
+                    <?php if (isset($show[$i])) : ?>
+                        <li><?= var_dump($user->cartHistoricdetails($product['id_order'])) ?></li>
+                    <?php endif; ?>
+                </ul>
+                <?php $i++; ?>
+            </li>
+
+        <?php endforeach; ?>
+    </ul>
