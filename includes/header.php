0c795b25efc097ff4120e343b18bc5ba1c30d738
huongmay-nguyenphuoc <huongmay-nguyenphuoc@laplateforme.io>
2021-02-17 14:58:05 +0100

    debut header

--- includes/header.php
+++ includes/header.php
@@ -6,9 +6,28 @@
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" type="text/css" media="screen" href="../style/boutique.css">
+    <link rel="stylesheet" type="text/css" media="screen" href="../style/header.css">
     <title>Gamebusters - $title</title>
 </head>
 <body>
 <header>
-header
+    <nav>
+        <div>
+        </div>
+        <div id="navClosed">
+                <input type="button" id="toggle" value="V">
+                <div id="navExpand">
+                    <a href="../index.php">Title Screen</a>
+                    <a href="../pages/souscategorie.php?cat=nintendo">Nintendo Island</a>
+                    <a href="../pages/souscategorie.php?cat=playstation">Playstation Island</a>
+                    <a href="../pages/souscategorie.php?cat=xbox">Xbox Island</a>
+                </div>
+        </div>
+        <div>
+            <a href="profil.php">Account</a>
+            <a href="shopcart.php">Cart</a>
+            <a href="search.php">Search</a>
+        </div>
+    </nav>
+
 </header>
--- pages/categorie.php
+++ pages/categorie.php
@@ -1,20 +1,20 @@
 <?php require_once('../includes/header.php'); ?>
 
 
-<main>
-    <article>
-        <div>
-            <a href="souscategorie.php?cat=nintendo">Nintendo</a>
-        </div>
-        <div>
-            <a href="souscategorie.php?cat=playstation">Playstation</a>
-        </div>
-    </article>
-    <article>
-        <div>
-            <a href="souscategorie.php?cat=xbox">xBox</a>
-        </div>
-    </article>
-</main>
+    <main>
+        <article>
+            <div>
+                <a class="catLink" href="souscategorie.php?cat=nintendo">Nintendo</a>
+            </div>
+            <div>
+                <a class="catLink" href="souscategorie.php?cat=playstation">Playstation</a>
+            </div>
+        </article>
+        <article>
+            <div>
+                <a class="catLink" href="souscategorie.php?cat=xbox">xBox</a>
+            </div>
+        </article>
+    </main>
 
 <?php require_once('../includes/footer.php'); ?>
