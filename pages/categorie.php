8bcf2846243f1ae874bbd7fc56d2df69a1fa0563
huongmay-nguyenphuoc <huongmay-nguyenphuoc@laplateforme.io>
2021-02-23 10:31:02 +0100

    cat

--- pages/categorie.php
+++ pages/categorie.php
@@ -16,9 +16,10 @@
             <div class="catDiv" id="catDiv3">
                 <a class="catLink" href="souscategorie.php?cat=xbox"><p>xBox</p></a>
             </div>
-            <div  class="catDiv" id="catDiv4">
-                <div>
-                    <p>Hello, stranger! Which island<br> will you visit now? Choose wisely ...</p>
+            <div  class="catDiv catDiv4">
+                <div class="text-box">
+                    <p>Hello, stranger! Which island</p>
+                    <p>will you visit now? Choose wisely ...</p>
                     <p><span style="color: red">Red</span> is the color of passion,</p>
                     <p><span style="color: lightgreen">Green</span> is the color of hope,</p>
                     <p><span style="color: lightskyblue">Blue</span> is the color of courage.</p>
--- pages/souscategorie.php
+++ pages/souscategorie.php
@@ -1,18 +1,42 @@
 <?php
 /*Gestion erreurs des infos dans l'URL*/
 $cats = array('nintendo', 'xbox', 'playstation');
-if (in_array(($_GET['cat']),$cats)) {
+if (in_array(($_GET['cat']), $cats)) {
     $cat = htmlspecialchars($_GET['cat']);
 } else {
     header('location:404.php');
 }
+
+$title = $cat;
 ?>
+<?php require_once('../includes/header.php'); ?>
 
-<head>
-    <title><?= $cat?></title>
-</head>
-
-<h5><?= $cat?></h5>
-<a href="boutique.php?cat=<?=$cat?>&subcat=games">Games</a>
-<a href="boutique.php?cat=<?=$cat?>&subcat=consoles">Consoles</a>
-<a href="boutique.php?cat=<?=$cat?>&subcat=secondhand">Second Hand</a>
+<main>
+    <article>
+        <div>
+            <a href="boutique.php?cat=<?= $cat ?>&subcat=games">Games</a>
+        </div>
+        <div>
+            <a href="boutique.php?cat=<?= $cat ?>&subcat=consoles">Consoles</a>
+        </div>
+        <div>
+            <a href="boutique.php?cat=<?= $cat ?>&subcat=secondhand">Second Hand</a>
+        </div>
+    </article>
+    <article>
+        <div></div>
+        <div class="text-box text-box2">
+            <p>So you chose the <span><b><?= $cat ?></b></span> island?</p>
+            <?php if ($cat = 'nintendo') :?>
+            <p>This island is full of love and passion!</p>
+            <p>You will be welcome warmly and heartily.</p>
+            <?php elseif ($cat = 'xbox') :?>
+                <p>This island is full of love and passion!</p>
+                <p>You will be welcome warmly and heartily.</p>
+            <?php elseif ($cat = 'playstation') :?>
+                <p>This island is for the brave</p>
+                <p>You will be welcome warmly and heartily.</p>
+            <?php endif;?>
+        </div>
+    </article>
+</main>
