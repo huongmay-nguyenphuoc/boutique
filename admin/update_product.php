80549962507af9d3713ec697b4f447e929084fcf
huongmay-nguyenphuoc <huongmay-nguyenphuoc@laplateforme.io>
2021-03-02 12:03:08 +0100

    update image

--- admin/update_product.php
+++ admin/update_product.php
@@ -1,5 +1,5 @@
 <?php
-
+$title = 'update Product';
 require_once '../classes/admin.php';
 require_once '../classes/user.php';
 require_once '../classes/product.php';
@@ -14,7 +14,8 @@ if (!isset($_SESSION['user']) or $_SESSION['user']->getStatus() != 1) {
     if (isset($_POST['submit'])) {
         $admin = new admin;
 
-        if (isset($_FILES)) {
+
+        if (isset($_POST['fileToUpload'])) {
             /*Check Image*/
             $target_dir = "../productPics/";
             $target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
@@ -39,6 +40,8 @@ if (!isset($_SESSION['user']) or $_SESSION['user']->getStatus() != 1) {
                 && $imageFileType != "gif") {
                 $errors[] = "Only JPG, JPEG, PNG & GIF pictures are allowed.";
             }
+        } else {
+            $image = $product['image'];
         }
 
 
@@ -46,8 +49,10 @@ if (!isset($_SESSION['user']) or $_SESSION['user']->getStatus() != 1) {
         if (empty($errors)) {
 
 //move picture in repository : productPics
-            if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
-                $image = htmlspecialchars(basename($_FILES["fileToUpload"]["name"]));
+            if (isset($_POST['fileToUpload'])) {
+                if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
+                    $image = htmlspecialchars(basename($_FILES["fileToUpload"]["name"]));
+                }
             }
             $reference = htmlspecialchars($_POST['reference']);
             $title = htmlspecialchars($_POST['title']);
@@ -60,124 +65,98 @@ if (!isset($_SESSION['user']) or $_SESSION['user']->getStatus() != 1) {
             $id_product = $_POST['id_product'];
 
 
-                $admin->update($reference, $category, $subcat, $title, $description, $shortdesc, $price, $stock, $id_product);
-            if (isset($image)) {
-                $admin->updatePic($image);
-            }
+            $admin->update($reference, $category, $subcat, $title, $image, $description, $shortdesc, $price, $stock, $id_product);
+
             $success = "Product has been updated<a href='produits.php'>Tous les produits</a>";
 
 
         }
-    }
 
+    }
 }
 ?>
 
 
-<<<<<<< Updated upstream
 <?php include 'includes/header.php'; ?>
-<main>
-=======
-    <html>
+    <main>
 
-<?php include 'includes/header.php'; ?>
-    <!--Alerte (erreur ou succès)-->
-<?php if (!empty($errors)): ?>
-    <div>
-        <?php foreach ($errors as $error) {
-            echo '<p class="red-text">' . $error . '</p>';
-        }
-        ?>
-    </div>
-<?php elseif (isset($success)): ?>
-    <div>
-        <p class="white-text"><?php echo $success; ?></p>
-    </div>
-<?php endif; ?>
-<body>
+        <h1> Update form </h1>
 
->>>>>>> Stashed changes
+        <section class="form">
 
-<h1> Update form </h1>
+            <form method="post" enctype="multipart/form-data"
+                  action="update_product.php?id_product=<?= $_GET["id_product"] ?>">
 
-    <section class="form">
+                <label for="id_product">id_product</label><br>
+                <input name="id_product" value="<?php echo $_GET["id_product"]; ?>" readonly="readonly"><br/><br/>
 
-<form method="post" enctype="multipart/form-data" action="update_product.php?id_product=<?= $_GET["id_product"] ?>">
-
-    <label for="id_product">id_product</label><br>
-    <input name="id_product" value="<?php echo $_GET["id_product"]; ?>" readonly="readonly"><br/><br/>
-
-    <label for="reference">reference</label><br>
-    <input type="text" id="reference" name="reference" value="<?= $product['reference'] ?>"> <br><br>
+                <label for="reference">reference</label><br>
+                <input type="text" id="reference" name="reference" value="<?= $product['reference'] ?>"> <br><br>
 
 
-    <label for="category">category</label><br>
-    <select name="category" id="category">
-        <option selected value="<?= $product['category'] ?>"> <?= $product['category'] ?> </option>
-        <option value="nintendo"> nintendo</option>
-        <option value="xbox"> xbox</option>
-        <option value="playstation"> playstation</option>
-    </select><br><br>
+                <label for="category">category</label><br>
+                <select name="category" id="category">
+                    <option selected value="<?= $product['category'] ?>"> <?= $product['category'] ?> </option>
+                    <option value="nintendo"> nintendo</option>
+                    <option value="xbox"> xbox</option>
+                    <option value="playstation"> playstation</option>
+                </select><br><br>
 
-    <label for="subcat">subcategory</label><br>
-    <select name="subcat" id=subcat">
-        <option selected value="<?= $product['subcategory'] ?>"> <?= $product['subcategory'] ?> </option>
-        <option value="games"> games</option>
-        <option value="secondhand"> secondhand</option>
-        <option value="consoles"> consoles</option>
-    </select><br><br>
-
-
-    <label for="title">title</label><br>
-    <input type="text" id="title" name="title" placeholder="product title" value="<?= $product['title'] ?>"> <br><br>
-
-    <label for="description">description</label><br>
-    <textarea name="description" id="description"
-              placeholder="product description"><?= $product['description'] ?></textarea><br><br>
-
-    <label for="description">Short description</label><br>
-    <textarea name="shortdesc" id="shortdesc"
-              placeholder="product short description"><?= $product['shortdesc'] ?></textarea><br><br>
-
-    <label for="file">picture</label><br>
-    <input type="file" name="fileToUpload" id="fileToUpload"><br><br>
-
-    <label for="price">price</label><br>
-<<<<<<< Updated upstream
-    <input type="text" id="price" name="price" placeholder="product price" pattern="[0-9]{2}" title="2 nombres requis : 0-9"   value="<?= $product['price'] ?>"><br><br>
-
-    <label for="stock">stock</label><br>
-    <input type="text" id="stock" name="stock" placeholder="product stock"  pattern="[0-9]{2}" title="2 nombres requis : 0-9" value="<?= $product['stock'] ?>"><br><br>
-=======
-    <input type="text" id="price" name="price" placeholder="product price" pattern="[0-9]{5}"
-           title="5 chiffres requis : 0-9" value="<?= $product['price'] ?>"><br><br>
-
-    <label for="stock">stock</label><br>
-    <input type="text" id="stock" name="stock" placeholder="product stock" pattern="[0-9]{5}"
-           title="5 chiffres requis : 0-9" value="<?= $product['stock'] ?>"><br><br>
->>>>>>> Stashed changes
-
-    <button class="btn waves-effect waves-light black" type="submit" name="submit">
-        <i class="material-icons right">send</i>
-    </button>
-
-</form>
+                <label for="subcat">subcategory</label><br>
+                <select name="subcat" id=subcat">
+                    <option selected value="<?= $product['subcategory'] ?>"> <?= $product['subcategory'] ?> </option>
+                    <option value="games"> games</option>
+                    <option value="secondhand"> secondhand</option>
+                    <option value="consoles"> consoles</option>
+                </select><br><br>
 
 
-    <!--Alerte (erreur ou succès)-->
-<?php if (!empty($errors)): ?>
-    <div>
-        <?php foreach ($errors as $error) {
-            echo '<p class="red-text">' . $error . '</p>';
-        }
-        ?>
-    </div>
-<?php elseif (isset($success)): ?>
-    <div>
-        <p class="white-text"><?php echo $success; ?></p>
-    </div>
-<?php endif; ?>
-    </section>
-</main>
+                <label for="title">title</label><br>
+                <input type="text" id="title" name="title" placeholder="product title" value="<?= $product['title'] ?>">
+                <br><br>
+
+                <label for="description">description</label><br>
+                <textarea name="description" id="description"
+                          placeholder="product description"><?= $product['description'] ?></textarea><br><br>
+
+                <label for="description">Short description</label><br>
+                <textarea name="shortdesc" id="shortdesc"
+                          placeholder="product short description"><?= $product['shortdesc'] ?></textarea><br><br>
+
+                <label for="file">picture</label><br>
+                <input type="file" name="fileToUpload" id="fileToUpload"><br><br>
+
+                <label for="price">price</label><br>
+
+                <input type="text" id="price" name="price" placeholder="product price" pattern="[0-9]{2}"
+                       title="2 nombres requis : 0-9" value="<?= $product['price'] ?>"><br><br>
+
+                <label for="stock">stock</label><br>
+                <input type="text" id="stock" name="stock" placeholder="product stock" pattern="[0-9]{2}"
+                       title="2 nombres requis : 0-9" value="<?= $product['stock'] ?>"><br><br>
+
+                <button class="btn waves-effect waves-light black" type="submit" name="submit">
+                    <i class="material-icons right">send</i>
+                </button>
+
+            </form>
+        </section>
+
+        <section>
+            <!--Alerte (erreur ou succès)-->
+            <?php if (!empty($errors)): ?>
+                <div>
+                    <?php foreach ($errors as $error) {
+                        echo '<p class="red-text">' . $error . '</p>';
+                    }
+                    ?>
+                </div>
+            <?php elseif (isset($success)): ?>
+                <div>
+                    <p class="white-text"><?php echo $success; ?></p>
+                </div>
+            <?php endif; ?>
+        </section>
+    </main>
 
 <?php include 'includes/footer.php'; ?>
