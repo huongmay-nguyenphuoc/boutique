54252cfb81567fddf0b5a931d50b2c255c2c75f1
Cordial <alicia.cordial@laplateforme.io>
2021-03-08 09:06:36 +0100

    review.sql

--- review.sql
+++ review.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Hôte : localhost:3306
--- Généré le : ven. 05 mars 2021 à 15:32
+-- Généré le : lun. 08 mars 2021 à 08:05
 -- Version du serveur :  5.7.30
 -- Version de PHP : 7.4.9
 
@@ -30,18 +30,21 @@ CREATE TABLE `review` (
   `id_review` int(11) NOT NULL,
   `id_member` int(11) DEFAULT NULL,
   `message_review` varchar(255) NOT NULL,
-  `date_review` date NOT NULL
+  `date_review` date NOT NULL,
+  `note` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
 -- Déchargement des données de la table `review`
 --
 
-INSERT INTO `review` (`id_review`, `id_member`, `message_review`, `date_review`) VALUES
-(1, 2, 'sdsdsdsd', '2021-03-03'),
-(2, 3, 'dffdf', '2021-03-03'),
-(3, 3, 'LOVE', '2021-03-04'),
-(4, 3, 'LOVE', '2021-03-04');
+INSERT INTO `review` (`id_review`, `id_member`, `message_review`, `date_review`, `note`) VALUES
+(1, 2, 'sdsdsdsd', '2021-03-03', 0),
+(2, 3, 'dffdf', '2021-03-03', 0),
+(3, 3, 'LOVE', '2021-03-04', 0),
+(4, 3, 'LOVE', '2021-03-04', 0),
+(5, 1, 'COOL', '2021-03-07', 5),
+(6, 1, 'good', '2021-03-07', 5);
 
 --
 -- Index pour les tables déchargées
@@ -61,7 +64,7 @@ ALTER TABLE `review`
 -- AUTO_INCREMENT pour la table `review`
 --
 ALTER TABLE `review`
-  MODIFY `id_review` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `id_review` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
